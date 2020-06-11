26b5ddc581014721a8e249608a6cc34837c23421
Nynu <adrianszarnowski8@gmail.com>
2020-06-11 18:25:25 +0200

    Finished get section of the API

--- endpoints/getMovie.js
+++ endpoints/getMovie.js
@@ -2,41 +2,81 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs").promises;
 
-const databasePath = "data/db.json";
+const databasePath = "./data/db.json";
 
 router.get("/getmovie", async (req, res) => {
   try {
     const rawMovieDatabase = await fs.readFile(databasePath);
     const movieList = JSON.parse(rawMovieDatabase).movies;
 
-    console.log(req.query);
-    let responseData = {};
-    if (req.query.genres) {
-      //handle genres search
-    } else {
-      if (req.query.duration) {
-        let duration = req.query.duration;
-        let filteredMovieList = movieList.filter((movie) => {
+    let requestedDuration = req.query.duration;
+    let requestedGenres = req.query.genres;
+    let responseData;
+    if (requestedGenres) {
+      //get all movies that have any genre that we're looking for
+      responseData = movieList.filter((m) =>
+        requestedGenres.some((g) => m.genres.includes(g))
+      );
+
+      //filter movies that don't match requested duration
+      if (requestedDuration) {
+        responseData = responseData.filter(
+          (m) =>
+            m.runtime > requestedDuration - 10 &&
+            m.runtime < parseInt(requestedDuration) + 10
+        );
+      }
+
+      responseData.sort((a, b) => {
+        const s =
+          b.genres.filter((g) => requestedGenres.includes(g)).length -
+          a.genres.filter((g) => requestedGenres.includes(g)).length;
+
+        //both movies have the same number of genres, sort by genre weight
+        if (s == 0) {
           return (
-            movie.runtime > duration - 10 &&
-            movie.runtime < parseInt(duration) + 10
+            calculateGenreValue(requestedGenres, a.genres) -
+            calculateGenreValue(requestedGenres, b.genres)
           );
-        });
+        }
+
+        return s;
+      });
+    } else {
+      if (requestedDuration) {
+        //only duration parameter provided, return movie from duration range
+        let filteredMovieList = movieList.filter(
+          (m) =>
+            m.runtime > requestedDuration - 10 &&
+            m.runtime < parseInt(requestedDuration) + 10
+        );
 
         responseData =
           filteredMovieList[
             Math.floor(Math.random() * filteredMovieList.length)
           ];
       } else {
-        responseData =
-          movieDatabase[Math.floor(Math.random() * movieDatabase.length)];
+        //no parameters provided, return a random movie
+        responseData = movieList[Math.floor(Math.random() * movieList.length)];
       }
     }
 
     res.json(responseData);
   } catch (e) {
     console.error(e);
+    res.status(500).send();
   }
 });
 
 module.exports = router;
+
+calculateGenreValue = (requestedGenres, movieGenres) => {
+  //we'll assign each genre a value equal to it's index and sum them up
+  //since the lowest index is the most important, we're looking for the lowest value
+  let sum = 0;
+  for (const genre of movieGenres) {
+    if (requestedGenres.includes(genre)) sum += requestedGenres.indexOf(genre);
+  }
+
+  return sum;
+};
