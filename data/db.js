a56634bfc50f172a5b1f512eb7a5613336a49533
Nynu <adrianszarnowski8@gmail.com>
2020-06-12 16:14:26 +0200

    Added separate file for database communication

---
+++ data/db.js
@@ -0,0 +1,35 @@
+const fs = require("fs").promises;
+const filePath = "./data/db.json";
+
+module.exports.getMovies = async () => {
+  try {
+    const rawData = await fs.readFile(filePath);
+    return JSON.parse(rawData).movies;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
+
+module.exports.getGenres = async () => {
+  try {
+    const rawData = await fs.readFile(filePath);
+    return JSON.parse(rawData).genres;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
+
+module.exports.addMovie = async (movie) => {
+  try {
+    const rawData = await fs.readFile(filePath);
+    let data = JSON.parse(rawData);
+    movie.id = data.movies[data.movies.length - 1].id++;
+    data.movies.push(movie);
+    await fs.writeFile(filePath, JSON.stringify(data));
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
