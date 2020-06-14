09b23992ada2275ea3bf5b3134f047843a9cf1dc
Nynu <adrianszarnowski8@gmail.com>
2020-06-14 20:39:10 +0200

    Fixed ID numering when adding

--- data/db.js
+++ data/db.js
@@ -25,9 +25,10 @@ module.exports.addMovie = async (movie) => {
   try {
     const rawData = await fs.readFile(filePath);
     let data = JSON.parse(rawData);
-    movie.id = data.movies[data.movies.length - 1].id++;
+    movie.id = data.movies[data.movies.length - 1].id + 1;
     data.movies.push(movie);
     await fs.writeFile(filePath, JSON.stringify(data));
+    return true;
   } catch (e) {
     console.error(e);
     return false;
