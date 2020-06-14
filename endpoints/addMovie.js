22b4c9ddffa3b95f0c8aeaf3dbd8ddb4c06eb0fc
Nynu <adrianszarnowski8@gmail.com>
2020-06-14 17:22:32 +0200

    Updated everything

--- endpoints/addMovie.js
+++ endpoints/addMovie.js
@@ -1,8 +1,87 @@
 const express = require("express");
+const { check, validationResult, oneOf } = require("express-validator");
 const router = express.Router();
 
-router.post("/addMovie", async (req, res) => {
-  throw "Not implemented";
-});
+const { getGenres, addMovie } = require("../data/db");
+
+router.post(
+  "/addmovie",
+  [
+    check("genres").custom(async (genres) => {
+      if (genres) {
+        const availableGenres = await getGenres();
+        if (availableGenres) {
+          let invalidGenres = genres.filter(
+            (g) => !availableGenres.includes(g)
+          );
+          if (invalidGenres.length > 0) {
+            throw `${invalidGenres.join(", ")} are not valid genres`;
+          }
+        }
+      } else {
+        throw "Genres can't be empty";
+      }
+    }),
+    check("title")
+      .exists()
+      .withMessage("Title can't be empty")
+      .bail()
+      .isLength({ max: 255 })
+      .withMessage("Title can't be longer than 255 characters"),
+    check("year")
+      .exists()
+      .withMessage("Year can't be empty")
+      .bail()
+      .isNumeric()
+      .withMessage("Year has to be a number"),
+    check("runtime")
+      .exists()
+      .withMessage("Runtime can't be empty")
+      .bail()
+      .isNumeric()
+      .withMessage("Runtime has to be a number"),
+    check("director")
+      .exists()
+      .withMessage("Director can't be empty")
+      .bail()
+      .isLength({ max: 255 })
+      .withMessage("Director can't be longer than 255 characters"),
+    oneOf([
+      check("actors").isEmpty(),
+      check("actors").isString().withMessage("Actors have to be a string"),
+    ]),
+    oneOf([
+      check("plot").isEmpty(),
+      check("plot").isString().withMessage("Plot has to be a string"),
+    ]),
+    oneOf([
+      check("posterUrl").isEmpty(),
+      check("posterUrl").isString().withMessage("posterUrl has to be a string"),
+    ]),
+  ],
+  async (req, res) => {
+    try {
+      validationResult(req).throw();
+      if (
+        await addMovie({
+          title: req.body.title,
+          year: req.body.year,
+          runtime: req.body.runtime,
+          genres: req.body.genres,
+          directors: req.body.directors,
+          actors: req.body.actors ?? "",
+          plot: req.body.plot ?? "",
+          posterUrl: req.body.posterUrl ?? "",
+        })
+      ) {
+        res.json("success");
+      } else {
+        res.status(500).end();
+      }
+    } catch (e) {
+      res.status(400).json(e.errors);
+    }
+  }
+);
 
 module.exports = router;
--- endpoints/getMovie.js
+++ endpoints/getMovie.js
@@ -8,55 +8,11 @@ router.get("/getmovie", async (req, res) => {
     const movieList = await getMovies();
     let requestedDuration = req.query.duration;
     let requestedGenres = req.query.genres;
-    let responseData;
-    if (requestedGenres) {
-      //get all movies that have any genre that we're looking for
-      responseData = movieList.filter((m) =>
-        requestedGenres.some((g) => m.genres.includes(g))
-      );
-
-      //filter movies that don't match requested duration
-      if (requestedDuration) {
-        responseData = responseData.filter(
-          (m) =>
-            m.runtime > requestedDuration - 10 &&
-            m.runtime < parseInt(requestedDuration) + 10
-        );
-      }
-
-      responseData.sort((a, b) => {
-        const s =
-          b.genres.filter((g) => requestedGenres.includes(g)).length -
-          a.genres.filter((g) => requestedGenres.includes(g)).length;
-
-        //both movies have the same number of genres, sort by genre weight
-        if (s == 0) {
-          return (
-            calculateGenreValue(requestedGenres, a.genres) -
-            calculateGenreValue(requestedGenres, b.genres)
-          );
-        }
-
-        return s;
-      });
-    } else {
-      if (requestedDuration) {
-        //only duration parameter provided, return movie from duration range
-        let filteredMovieList = movieList.filter(
-          (m) =>
-            m.runtime > requestedDuration - 10 &&
-            m.runtime < parseInt(requestedDuration) + 10
-        );
-
-        responseData =
-          filteredMovieList[
-            Math.floor(Math.random() * filteredMovieList.length)
-          ];
-      } else {
-        //no parameters provided, return a random movie
-        responseData = movieList[Math.floor(Math.random() * movieList.length)];
-      }
-    }
+    let responseData = await selectMovies(
+      movieList,
+      requestedDuration,
+      requestedGenres
+    );
 
     res.json(responseData);
   } catch (e) {
@@ -66,6 +22,67 @@ router.get("/getmovie", async (req, res) => {
 });
 
 module.exports = router;
+
+module.exports.selectMovies = selectMovies = async (
+  movieList,
+  requestedDuration,
+  requestedGenres
+) => {
+  //there are no movies on the list, we can't get any :(
+  if (movieList.length === 0) return false;
+
+  if (requestedGenres) {
+    //get all movies that have any genre that we're looking for
+    let data = movieList.filter((m) =>
+      requestedGenres.some((g) => m.genres.includes(g))
+    );
+
+    //filter movies that don't match requested duration
+    if (requestedDuration) {
+      data = data.filter(
+        (m) =>
+          m.runtime >= requestedDuration - 10 &&
+          m.runtime <= parseInt(requestedDuration) + 10
+      );
+    }
+
+    data.sort((a, b) => {
+      const s =
+        b.genres.filter((g) => requestedGenres.includes(g)).length -
+        a.genres.filter((g) => requestedGenres.includes(g)).length;
+
+      //both movies have the same number of genres, sort by genre weight
+      if (s == 0) {
+        return (
+          calculateGenreValue(requestedGenres, a.genres) -
+          calculateGenreValue(requestedGenres, b.genres)
+        );
+      }
+
+      return s;
+    });
+
+    return data.length > 0 ? data : false;
+  } else {
+    if (requestedDuration) {
+      //only duration parameter provided, return movie from duration range
+      let filteredMovieList = movieList.filter(
+        (m) =>
+          m.runtime >= requestedDuration - 10 &&
+          m.runtime <= parseInt(requestedDuration) + 10
+      );
+
+      return filteredMovieList.length > 0
+        ? filteredMovieList[
+            Math.floor(Math.random() * filteredMovieList.length)
+          ]
+        : false;
+    } else {
+      //no parameters provided, return a random movie
+      return movieList[Math.floor(Math.random() * movieList.length)];
+    }
+  }
+};
 
 calculateGenreValue = (requestedGenres, movieGenres) => {
   //we'll assign each genre a value equal to it's index and sum them up
